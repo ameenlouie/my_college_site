fcd6916c75011efc657ec5b1a959b3b8d6b89315
Ameen <ameenlouie@gmail.com>
2025-09-26 10:20:27 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My College - Home</title>
+</head>
+<body>
+    <header>
+        <h1>My College</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About Us</a></li>
+            </ul>
+        </nav>
+    </header>
